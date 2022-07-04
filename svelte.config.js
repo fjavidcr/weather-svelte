b80d4d192739480aa21d50eb2a5270258433d179
javier_delcastillo_contractor <javier.delcastillo.contractor@bbva.com>
2022-07-04 12:40:49 +0200

    custom base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter: adapter()
 		adapter: adapter({
       fallback: '200.html'
-    })
+    }),
+		paths: {
+			base: dev ? '' : '/your-repo-name',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'app', 'internal', etc.
+		appDir: 'app',
 	}
 };
 
